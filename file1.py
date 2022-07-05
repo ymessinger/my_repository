e4d9f000814007deeeb291eb6b7f3e2f0614718f
ymessinger <108732716+ymessinger@users.noreply.github.com>
2022-07-05 06:08:22 -0500

    Add files via upload

---
+++ file1.py
@@ -0,0 +1,5 @@
+years = [2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2010]
+durations = [103, 101, 99, 100, 100, 95, 95, 96, 93, 90]
+movie_dict = {"years": years, "durations": durations}
+print(movie_dict)
+import pandas as pd
